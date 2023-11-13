6140cb50606063789cdc3460d12dd6f6146d21b4
vishnusureshperumbavoor <vishnusureshperumbavoor@gmail.com>
2023-11-13 16:16:33 +0530

    grayscale imaging

--- client/src/Components/Main.jsx
+++ client/src/Components/Main.jsx
@@ -15,21 +15,7 @@ function Main() {
         const { patientName, pixelData } = response.data;
         setPatientName(patientName);
         const arr = new Uint8ClampedArray(pixelData.data);
-        // const blob = new Blob([arr.buffer], { type: "image/png" });
-        let image = new Image(arr,200,200)
-        const canvas = canvasRef.current;
-        const ctx = canvas.getContext("2d");
-        ctx.putImageData(image,20,20)
-        // const url = URL.createObjectURL(blob);
-        // setImageSrc(url);
-
-        // const img = new Image();
-        // img.onload = () => {
-        //   canvas.width = img.width;
-        //   canvas.height = img.height;
-        //   ctx.drawImage(img, 0, 0);
-        // };
-        // img.src = url;
+        console.log(arr);
       })
       .catch((err) => {
         console.log("error");
--- client/src/Components/Second.jsx
+++ client/src/Components/Second.jsx
@@ -7,17 +7,49 @@ function Second() {
     const myCanvas = document.getElementById("myCanvas");
     const myContext = myCanvas.getContext("2d");
     const img = new Image();
+    img.crossOrigin = "anonymous";
     img.src = imageURL;
     img.onload = () => {
       myContext.drawImage(img, 0, 0);
+      const imageData = myContext.getImageData(
+        0,
+        0,
+        myCanvas.width,
+        myCanvas.height
+      );
+      const arr = imageData.data;
+      for (var i = 0; i < arr.length; i += 4) {
+        var brightness = 0.34 * arr[i] + 0.5 * arr[i + 1] + 0.16 * arr[i + 2];
+        arr[i] = brightness;
+        arr[i + 1] = brightness;
+        arr[i + 2] = brightness;
+      }
+
+      console.log(arr);
+
+      const myCanvas2 = document.getElementById("myCanvas2");
+      const myContext2 = myCanvas2.getContext("2d");
+      const newImageData = new ImageData(
+        new Uint8ClampedArray(arr),
+        myCanvas.width,
+        myCanvas.height
+      );
+      myContext2.putImageData(newImageData, 0, 0);
     };
   }, []);
 
   return (
     <div>
-      canvas
+      <h1>Grayscale</h1>
       <canvas
         id="myCanvas"
+        width="400"
+        height="300"
+        style={{ border: "1px solid #000000" }}
+      ></canvas>
+      &nbsp;
+      <canvas
+        id="myCanvas2"
         width="400"
         height="300"
         style={{ border: "1px solid #000000" }}
